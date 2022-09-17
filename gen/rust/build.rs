ee43a96a9e4a0398e48095a98f91366a44504cca
pyama2000 <takahiko-yamashita@retty.me>
2022-09-17 11:12:26 +0900

    add: Build gRPC clients and servers from protobuf

---
+++ gen/rust/build.rs
@@ -0,0 +1,36 @@
+const PROTO_ROOT_DIR: &str = "../../proto";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR")?);
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("kintai_service_descriptor.bin"))
+        .compile(
+            &[format!("{PROTO_ROOT_DIR}/kintai/v1/kintai_service.proto")],
+            &[PROTO_ROOT_DIR],
+        )?;
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("notification_service_descriptor.bin"))
+        .compile(
+            &[format!(
+                "{PROTO_ROOT_DIR}/notification/v1/notification_service.proto"
+            )],
+            &[PROTO_ROOT_DIR],
+        )?;
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("release_service_descriptor.bin"))
+        .compile(
+            &[format!(
+                "{PROTO_ROOT_DIR}/automation/v1/release_service.proto"
+            )],
+            &[PROTO_ROOT_DIR],
+        )?;
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("server_status_service_descriptor.bin"))
+        .compile(
+            &[format!(
+                "{PROTO_ROOT_DIR}/server_status/v1/server_status_service.proto"
+            )],
+            &[PROTO_ROOT_DIR],
+        )?;
+    Ok(())
+}
---
+++ gen/rust/src/lib.rs
@@ -0,0 +1,36 @@
+// tonic does not derive `Eq` for the gRPC message types, which causes a warning from Clippy. The
+// current suggestion is to explicitly allow the lint in the module that imports the protos.
+// Read more: https://github.com/hyperium/tonic/issues/1056
+#![allow(clippy::derive_partial_eq_without_eq)]
+
+pub mod kintai {
+    pub mod v1 {
+        tonic::include_proto!("kintai.v1");
+        pub const KINTAI_SERVICE_FILE_DESCRIPTOR_SET: &[u8] =
+            tonic::include_file_descriptor_set!("kintai_service_descriptor");
+    }
+}
+
+pub mod notification {
+    pub mod v1 {
+        tonic::include_proto!("notification.v1");
+        pub const NOTIFICATION_SERVICE_FILE_DESCRIPTOR_SET: &[u8] =
+            tonic::include_file_descriptor_set!("notification_service_descriptor");
+    }
+}
+
+pub mod automation {
+    pub mod v1 {
+        tonic::include_proto!("automation.v1");
+        pub const RELEASE_SERVICE_FILE_DESCRIPTOR_SET: &[u8] =
+            tonic::include_file_descriptor_set!("release_service_descriptor");
+    }
+}
+
+pub mod server_status {
+    pub mod v1 {
+        tonic::include_proto!("server_status.v1");
+        pub const SERVER_STATUS_SERVICE_DESCRIPTOR_SET: &[u8] =
+            tonic::include_file_descriptor_set!("server_status_service_descriptor");
+    }
+}
